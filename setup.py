4b097339c8d3b49cfe191ef6a466baf4eddec1de
Thanh Nguyen <thanhnguyen@kaist.ac.kr>
2025-01-31 11:47:52 +0000

    Correct setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
+import os
+
 from setuptools import find_packages, setup
+
 
 def requirements(fname):
     return [
         line.strip() for line in open(os.path.join(os.path.dirname(__file__), fname))
     ]
-    
-reqs = requirements("requirements.txt")    
-    
+
+
+reqs = requirements("requirements.txt")
+
 
 setup(
-    name='my_package',
-    description=('self-assess is a python package for self-revive the output of LLMs '
-                 'to improve the quality of the generated text.'),
-    version='0.1',
-    author='Thanh Nguyen',
+    name="my_package",
+    description=(
+        "self-assess is a python package for self-revive the output of LLMs "
+        "to improve the quality of the generated text."
+    ),
+    version="0.1",
+    author="Thanh Nguyen",
     packages=find_packages(),
-    install_requires=[] # Add your external package dependencies here or use `reqs` variable
+    install_requires=[],  # se `reqs` variable
 )
